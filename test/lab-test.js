13d8ffc3303fa4e4091bf42e62d4b24cdda14bdc
Anders Janmyr <anders@janmyr.com>
2014-01-15 17:40:46 +0100

    Added a bunch of promise tests

--- test/lab-test.js
+++ test/lab-test.js
@@ -4,10 +4,55 @@ var lab = require('../lib/lab');
 var expect = require('chai').expect;
 
 describe('lab', function() {
-    describe('delayedHello', function() {
+    describe('asyncHello', function() {
         it('calls callback with hello after a while', function(done) {
-            lab.delayedHello(function(err, hello) {
-                expect(hello).to.equal('Hello');
+            lab.asyncHello('Tapir', function(err, hello) {
+                expect(hello).to.equal('Hello Tapir');
+                done();
+            });
+        });
+    });
+
+    describe('asyncFail', function() {
+        it('calls callback with hello after a while', function(done) {
+            lab.asyncFail('Tapir', function(err, hello) {
+                expect(err).to.equal('I fail, therefore I am');
+                done();
+            });
+        });
+    });
+
+    describe('promisedHello', function() {
+        it('calls then with hello after a while', function(done) {
+            lab.promisedHello('Sloth').then(function(hello) {
+                expect(hello).to.equal('Hello Sloth');
+                done();
+            });
+        });
+    });
+
+    describe('promisedFail', function() {
+        it('calls then with hello after a while', function(done) {
+            lab.promisedFail('Sloth').catch(function(err) {
+                expect(err).to.equal('I fail, therefore I am');
+                done();
+            });
+        });
+    });
+
+    describe('nfcallHello', function() {
+        it('calls then with hello after a while', function(done) {
+            lab.nfcallHello('Aardvark').then(function(hello) {
+                expect(hello).to.equal('Hello Aardvark');
+                done();
+            });
+        });
+    });
+
+    describe('nbindHello', function() {
+        it('calls then with hello after a while', function(done) {
+            lab.nbindHello('Aardvark').then(function(hello) {
+                expect(hello).to.equal('Hello Aardvark');
                 done();
             });
         });
